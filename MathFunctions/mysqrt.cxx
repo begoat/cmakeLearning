05f3dd5635dd367f5e0b55deff150618d53654aa
William Huang <hylbegoat@gmail.com>
2017-08-03 08:42:31 +0800

    step2 successfully add a library without option

---
+++ MathFunctions/mysqrt.cxx
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <math.h> 
+#include <iostream>
+#include "MathFunctions.h"
+using namespace std;
+
+double mysqrt(double value){
+    cout<<"william's library";
+    return sqrt(value);
+}
